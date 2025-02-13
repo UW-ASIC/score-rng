799e6cd453a3d5a74b9fe1dd2b50b48b09e30fb9
Gerry Chen <83669998+gjrchen@users.noreply.github.com>
2025-02-12 23:08:51 -0500

    fix module definition

--- test/tb.v
+++ test/tb.v
@@ -22,7 +22,6 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-   wire debug;
 `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
@@ -45,7 +44,6 @@ tt_um_dino_score user_project(
       .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
-      .debug(debug)
   );
 
 endmodule
