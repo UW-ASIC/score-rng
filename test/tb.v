96b9ee5d0fa623263179084ec4f0a8c6dc9430a0
Gerry Chen <83669998+gjrchen@users.noreply.github.com>
2025-02-12 21:59:01 -0500

    fix tb module instantiation

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
- tt_um_dino_score (
+tt_um_dino_score score(
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
