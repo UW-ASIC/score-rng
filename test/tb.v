00a1429d208776fd35789c7e235d279e4d79fc65
Gerry Chen <83669998+gjrchen@users.noreply.github.com>
2025-02-12 22:07:00 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-tt_um_dino_score score(
+tt_um_dino_score user_project(
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
